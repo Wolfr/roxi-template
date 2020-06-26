ee31a22cff70fc620c3479c0d4d4d7da6919868e
Wolfr <johan.ronsse@gmail.com>
2020-06-26 23:52:45 +0200

    Fix import
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Router } from "roxi/routify";
   import { routes } from "../_roxi/.routify/routes";
-  import style from './css/global.css'
+  import './css/global.css'
 </script>
 
 <Router {routes} />asd123asdasd123
